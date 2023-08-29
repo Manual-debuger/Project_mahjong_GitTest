0e318ee7732ee2fa8e5f451c042c25e6c0a44353
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-08-29 16:28:22 +0800

    新增了沒用的AssetBundleCreator

---
+++ Assets/Scripts/Editor/AssetBundleCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateAssetBundleCreator
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    private static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectoryPath = "Assets/StreamingAssets";
+        try
+        {
+            if (!System.IO.Directory.Exists(assetBundleDirectoryPath))
+                System.IO.Directory.CreateDirectory(assetBundleDirectoryPath);
+            BuildPipeline.BuildAssetBundles(assetBundleDirectoryPath, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error: " + ex.Message);
+            throw ex;
+        }
+    }
+
+}

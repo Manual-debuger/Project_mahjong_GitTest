889bda275575f53d0270e02dd733996c2c25f939
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-09-06 22:21:07 +0800

    修復無法棄牌的問題

--- Assets/Scripts/FunctionalController/GameManager.cs
+++ Assets/Scripts/FunctionalController/GameManager.cs
@@ -235,6 +235,8 @@ public class GameManager : MonoBehaviour,IInitiable
         {
             _centralAreaController.SetWallCount(e.WallCount ?? -1);
             _playerControllers[CastAPIIndexToLocalIndex(this._playerIndex)].SetHandTiles(e.Tiles, e.PlayingIndex == this._playerIndex);
+            if (e.Actions != null)
+                _inGameUIController.ActionUISet(e.Actions);
             for (int i=0;i< e.Seats.Count();i++)
             {
                 debugMessage += ", Name: " + e.Seats[i].Nickname;
@@ -275,6 +277,7 @@ public class GameManager : MonoBehaviour,IInitiable
         {
             _centralAreaController.SetWallCount(e.WallCount ?? -1);
             _playerControllers[CastAPIIndexToLocalIndex(_playerIndex)].SetHandTiles(e.Tiles);
+            
             if (e.Actions != null)
                 _inGameUIController.ActionUISet(e.Actions);
             

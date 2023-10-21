1ad50af27eb8618dbc7a4ab6ea686c085fe72468
everydayplaycat <dada910517@gmail.com>
2023-10-22 05:28:20 +0800

    聊天室逐漸顯示(以人為單位)

--- Assets/Scripts/UIScripts/scrollrect.cs
+++ Assets/Scripts/UIScripts/scrollrect.cs
@@ -11,31 +11,55 @@ public class scrollrect : MonoBehaviour
     private TMP_Text textView;
     [SerializeField]
     private ScrollRect scrollControl;
+    public List<Tuple<String, String>> linesToDisplay = new();
+    public float displayDuration = 2.0f;
+    private bool isDisplaying = false;
 
     private void Start()
     {
     }
 
+    IEnumerator DisplayTextCoroutine()
+    {
+        isDisplaying = true;
+
+        while (linesToDisplay.Count!=0)
+        {
+            AddChatToText(linesToDisplay[0]);
+            linesToDisplay.RemoveAt(0);
+            yield return new WaitForSeconds(displayDuration);
+        }
+        isDisplaying = false;
+    }
     public void AddChat(List<Tuple<String, String>> text)
     {
-        string newcontent = "";
-        for (int i = text.Count - 1; i >= 0; i--)
+        for (int i = 0; i < text.Count; i++)
         {
-            newcontent += "\n";
-            newcontent += "<color=#FFFFFF>";
-            newcontent += text[i].Item1;
-            newcontent += "</color>";
-            newcontent += ":";
-            newcontent += "\n";
-            newcontent += text[i].Item2;
+            linesToDisplay.Add(text[i]);
         }
+        if (!isDisplaying)
+        {
+            StartCoroutine(DisplayTextCoroutine());
+        }
+    }
+
+    public void AddChatToText(Tuple<String, String> text)
+    {
+        string newcontent = "";
+        newcontent += "<color=#2491aa>";
+        newcontent += text.Item1;
+        newcontent += "</color>";
+        newcontent += ":";
+        newcontent += "\n";
+        newcontent += text.Item2;
+        newcontent += "\n";
         textView.text = textView.text + newcontent;
         StartCoroutine("ScrollToBottom");
     }
 
     IEnumerator ScrollToBottom()
     {
-        yield return new WaitForEndOfFrame();
+        yield return null;
         scrollControl.verticalNormalizedPosition = 0f;
     }
 }

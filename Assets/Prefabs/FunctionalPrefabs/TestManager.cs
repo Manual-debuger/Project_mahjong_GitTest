5cb4e6169aab2f14687067258b256348bb71c5fd
Manual-debuger <103013135+Manual-debuger@users.noreply.github.com>
2023-10-25 18:25:58 +0800

    Test SSE

--- Assets/Prefabs/FunctionalPrefabs/TestManager.cs
+++ Assets/Prefabs/FunctionalPrefabs/TestManager.cs
@@ -1,16 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Net.Http;
+using TMPro;
 using UnityEngine;
 
 public class TestManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private EffectController _effectController;
-
-    async void Start()
+    [SerializeField] private TextMeshProUGUI _textMeshPro;
+    void Start()
     {
-        var message = (await ChatGPTTool.CallChatGPT("introduce youself")).choices[0].message.content;
-        Debug.Log(message);
+        //var message = (await ChatGPTTool.CallChatGPT("introduce youself")).choices[0].message.content;
+        //Debug.Log(message);
+        var stream = new HttpClient().GetStreamAsync("https://localhost:7195/api/Test/SSE").Result;
+        using (var reader=new StreamReader(stream))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                Debug.Log(line + "\n");
+            }
+        }
+    }
+
+    private void EventSource_EventCommandExecuted(object sender, EventCommandEventArgs e)
+    {
+        _textMeshPro.text = e.Arguments.Keys.ToString();
     }
 
     // Update is called once per frame

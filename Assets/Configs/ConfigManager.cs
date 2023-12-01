0facc6ffd7ed039e446780b5cb44028f41e5547c
st97110 <47217662+st97110@users.noreply.github.com>
2023-12-01 22:45:23 +0800

    Fix ConfigManager

--- Assets/Configs/ConfigManager.cs
+++ Assets/Configs/ConfigManager.cs
@@ -1,13 +1,7 @@
 using Newtonsoft.Json;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
-using Unity.VisualScripting.FullSerializer;
-using UnityEditor.Purchasing;
 using UnityEngine;
-using UnityEngine.Networking;
 
 public class ConfigManager
 {
